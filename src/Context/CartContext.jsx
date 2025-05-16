2e7d12d8af2947daaf36d63cdc42ec06fcaa4755
unknown <josebarroso0887@digitalcollege.internal>
2025-05-16 16:31:22 -0300

    adição e remoção do card

---
+++ src/Context/CartContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useState } from "react"
+
+const CartContext = createContext()
+
+const CartProvider = ({ children }) => {
+
+    const [count, setCount] = useState(0)
+    function addToCart() {
+        setCount((count) => count + 1)
+    }
+    function removeFromCart() {
+        setCount((count) => count -1)
+    }
+    return (
+        <>
+            <CartContext.Provider value ={{count, addToCart, removeFromCart, setCount}}>
+                {children}
+            </CartContext.Provider>
+        </>
+    )
+}
+
+export { CartContext, CartProvider }
--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -1,40 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext} from 'react'
 import tenis from '../assets/tenis.svg'
+import { CartContext } from '../Context/CartContext'
 
-const produtos = [
-    {
-        produto: 'K-Swiss V8 - Masculino',
-        categoria: 'Tenis',
-        preco: 200,
-        img: tenis
-    },{
-        produto: 'K-Swiss V8 - Masculino',
-        categoria: 'Tenis',
-        preco: 200,
-        img: tenis
-    },{
-        produto: 'K-Swiss V8 - Masculino',
-        categoria: 'Tenis',
-        preco: 200,
-        img: tenis
-    },{
-        produto: 'K-Swiss V8 - Masculino',
-        categoria: 'Tenis',
-        preco: 200,
-        img: tenis
-    }
-]
+
 
 
 
 const Carrinho = () => {
-    const [count, setCount] = useState(0)
-    function addToCart(){
-        setCount((count) => count + 1)
-    }
-  return (
+   const { count, addToCart, removeFromCart, setCount} = useContext(CartContext)
+   
+   return (
     <>
-    <section className='bg-white p-8'>
+  <section className='bg-white p-8'>
         <table className='w-full font-inter'>
             <thead>
                 <tr className='flex text-[#474747]'>
@@ -59,11 +36,11 @@ const Carrinho = () => {
                     </td>
                     <td className='flex-1 text-center flex items-center flex-col justify-center gap-4'>
                         <div className=''>
-                            <button disabled={count <= 0} onClick= {() => setCount(count - 1)} className={`${count <= 0 ? "opacity-50 cursor-not-allowed ": "cursor-pointer" } border border-[#ccc] w-8 h-8 rounded-sm`}>-</button>
+                            <button disabled={count <= 0} onClick= {removeFromCart} className={`${count <= 0 ? "opacity-50 cursor-not-allowed ": "cursor-pointer" } border border-[#ccc] w-8 h-8 rounded-sm`}>-</button>
                             <span className='mx-2'>{count}</span>
-                            <button onClick= {() => setCount(count + 1)} className='border border-[#ccc] w-8 h-8 cursor-pointer rounded-sm'>+</button>
+                            <button onClick= {addToCart} className='border border-[#ccc] w-8 h-8 cursor-pointer rounded-sm'>+</button>
                         </div>
-                        <button className='text-[#474747] underline cursor-pointer'>Remover item</button>
+                        <button disabled={count == 0} onClick={()=>setCount(0)} className='text-[#474747] underline cursor-pointer'>Remover item</button>
                     </td>
                     <td className='flex-1 bg-blue-700'></td>
                     <td className='flex-1 bg-fuchsia-600'></td>
@@ -71,24 +48,7 @@ const Carrinho = () => {
             </tbody>
         </table>
     </section>
-    <section className=' ml-8'>
-        <h1 className='font-bold text-2xl mb-5'>Produtos Relacionados</h1>
-        <ul className='flex gap-20'>
-            {produtos.map((item)=> (
-                <li className='border border-[#ccc] p-2'>
-                    <div className='bg-[#E2E3FF] h-28 w-32 flex items-center justify-center rounded-sm '>
-                            <img src={item.img} alt={item.produto} />
-                    </div>
-                    <div>
-                        <h2>{item.categoria}</h2>
-                        <h2 className='text-xs'>{item.produto}</h2>
-                        <p>{item.preco}</p>
-                        <button onClick={()=>addToCart(item)} className='bg-green-700 w-[80%] rounded-md'>Add</button>
-                    </div>
-                </li>
-            ))}
-        </ul>
-    </section>
+    
     </>
   )
 }
